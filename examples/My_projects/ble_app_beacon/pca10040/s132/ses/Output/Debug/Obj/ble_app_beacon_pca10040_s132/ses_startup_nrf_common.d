915d5f95adff464a464302454ee1808b8a7f2cf4
MiguelOrtega-V <veoranmi@gmail.com>
2023-11-30 17:26:26 +0100

    Prueba coded - fail

--- examples/My_projects/ble_app_beacon/pca10040/s132/ses/Output/Debug/Obj/ble_app_beacon_pca10040_s132/ses_startup_nrf_common.d
+++ examples/My_projects/ble_app_beacon/pca10040/s132/ses/Output/Debug/Obj/ble_app_beacon_pca10040_s132/ses_startup_nrf_common.d
@@ -1,2 +1,2 @@
 Output/Debug/Obj/ble_app_beacon_pca10040_s132/ses_startup_nrf_common.o: \
- C:\nrf_sdk\modules\nrfx\mdk\ses_startup_nrf_common.s
+ C:\nrf_git\nrf\modules\nrfx\mdk\ses_startup_nrf_common.s
