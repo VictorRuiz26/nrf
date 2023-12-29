0955d1f43f1ef42c148e842786224e4bcd474f13
Victor Ruiz <victorcopero00@hotmail.com>
2023-12-29 17:00:16 +0100

    - Cambio a segundos de delay en lugar de minutos en entorno de compilación. - Manejo del tiempo de reposo usando un timer, y no un delay. Cada vez que salte el timer, se reinicializa el módulo advertising para aumentar major y minor y empezar de nuevo.

--- examples/My_projects/ble_app_beacon/main.c
+++ examples/My_projects/ble_app_beacon/main.c
@@ -1,51 +1,3 @@
-/**
- * Copyright (c) 2014 - 2021, Nordic Semiconductor ASA
- *
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form, except as embedded into a Nordic
- *    Semiconductor ASA integrated circuit in a product or a software update for
- *    such product, must reproduce the above copyright notice, this list of
- *    conditions and the following disclaimer in the documentation and/or other
- *    materials provided with the distribution.
- *
- * 3. Neither the name of Nordic Semiconductor ASA nor the names of its
- *    contributors may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
- *
- * 4. This software, with or without modification, must only be used with a
- *    Nordic Semiconductor ASA integrated circuit.
- *
- * 5. Any software provided in binary form under this license must not be reverse
- *    engineered, decompiled, modified and/or disassembled.
- *
- * THIS SOFTWARE IS PROVIDED BY NORDIC SEMICONDUCTOR ASA "AS IS" AND ANY EXPRESS
- * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL NORDIC SEMICONDUCTOR ASA OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
-/** @file
- *
- * @defgroup ble_sdk_app_beacon_main main.c
- * @{
- * @ingroup ble_sdk_app_beacon
- * @brief Beacon Transmitter Sample Application main file.
- *
- * This file contains the source code for an Beacon transmitter sample application.
- */
 
 #include <stdbool.h>
 #include <stdint.h>
@@ -56,6 +8,7 @@
 #include "nrf_sdh_ble.h"
 #include "ble_advdata.h"
 #include "app_timer.h"
+#include "nrf_drv_clock.h"
 #include "nrf_pwr_mgmt.h"
 #include "nrf_delay.h"
 
@@ -64,6 +17,7 @@
 #include "nrf_log_default_backends.h"
 
 #include <time.h>
+
 
 #define APP_BLE_CONN_CFG_TAG            1                                  /**< A tag identifying the SoftDevice BLE configuration. */
 
@@ -106,7 +60,6 @@
 #define UICR_ADDRESS                    0x10001080                         /**< Address of the UICR register used by this example. The major and minor versions to be encoded into the advertising data will be picked up from this location. */
 #endif
 
-uint8_t testCallback();
 
 static ble_gap_adv_params_t m_adv_params;                                  /**< Parameters to be passed to the stack when starting advertising. */
 static uint8_t              m_adv_handle = BLE_GAP_ADV_SET_HANDLE_NOT_SET; /**< Advertising handle used to identify an advertising set. */
@@ -249,24 +202,12 @@ static void advertising_start(void)
 {
     ret_code_t err_code;
 
-    NRF_LOG_INFO("Starting advertising");
     err_code = sd_ble_gap_adv_start(m_adv_handle, APP_BLE_CONN_CFG_TAG);
-    //APP_ERROR_CHECK(err_code);    
+    APP_ERROR_CHECK(err_code);    
 
     err_code = bsp_indication_set(BSP_INDICATE_ADVERTISING);
     APP_ERROR_CHECK(err_code);
 
-    /*
-    nrf_delay_ms(6000);
-
-    err_code = sd_ble_gap_adv_stop(m_adv_handle);
-    APP_ERROR_CHECK(err_code);
-    NRF_LOG_INFO("Stopping advertising and restart");
-
-    err_code = sd_ble_gap_adv_start(m_adv_handle, APP_BLE_CONN_CFG_TAG);
-    APP_ERROR_CHECK(err_code);
-
-    */
 }
 
 /**@brief Function for stopping advertising.
@@ -276,11 +217,11 @@ static void advertising_stop(void)
     ret_code_t err_code;
 
     err_code = sd_ble_gap_adv_stop(m_adv_handle);
-    //APP_ERROR_CHECK(err_code);
+    APP_ERROR_CHECK(err_code);
     NRF_LOG_INFO("Stopping advertising");
 
-    err_code = sd_ble_gap_adv_start(m_adv_handle, APP_BLE_CONN_CFG_TAG);
-    APP_ERROR_CHECK(err_code);
+    //err_code = sd_ble_gap_adv_start(m_adv_handle, APP_BLE_CONN_CFG_TAG);
+    //APP_ERROR_CHECK(err_code);
 }
 
 
@@ -307,11 +248,6 @@ static void ble_stack_init(void)
     APP_ERROR_CHECK(err_code);
 }
 
-uint8_t testCallback(){
-  NRF_LOG_INFO("CALLBACK LLAMADO OK!!");
-  return NRF_SUCCESS;
-}
-
 /**@brief Function for initializing logging. */
 static void log_init(void)
 {
@@ -328,13 +264,6 @@ static void leds_init(void)
     APP_ERROR_CHECK(err_code);
 }
 
-
-/**@brief Function for initializing timers. */
-static void timers_init(void)
-{
-    ret_code_t err_code = app_timer_init();
-    APP_ERROR_CHECK(err_code);
-}
 
 
 /**@brief Function for initializing power management.
@@ -359,6 +288,41 @@ static void idle_state_handle(void)
     }
 }
 
+// ************ DEFINICION TIMERS Y VARIABLES *************
+APP_TIMER_DEF(m_timer_ble);
+
+#define ADV_INTERVAL APP_TIMER_TICKS(SEGUNDOS_DELAY*1000)
+
+static void lfclk_config (void) 
+{
+  #ifndef SOFTDEVICE_PRESENT
+  ret_code_t err_code = nrf_drv_clock_init();
+  APP_ERROR_CHECK(err_code;)
+
+  nrf_drv_clock_lfclk_request(NULL);
+  #endif
+}
+
+static void app_timer_ble_interval_handler (void *p_context)
+{
+    // Inicializamos características advertisement
+    advertising_init();
+
+    advertising_start();
+}
+
+/**@brief Function for initializing timers. */
+static void timers_init(void)
+{
+    ret_code_t err_code = app_timer_init();
+    APP_ERROR_CHECK(err_code);
+
+    err_code = app_timer_create(&m_timer_ble, APP_TIMER_MODE_REPEATED, app_timer_ble_interval_handler);
+    APP_ERROR_CHECK(err_code);
+
+}
+// ********************************************************
+
 
 /**
  * @brief Function for application main entry.
@@ -366,43 +330,19 @@ static void idle_state_handle(void)
 int main(void)
 {
     // Initialize.
+    lfclk_config();
     log_init();
     timers_init();
     leds_init();
     power_management_init();
     ble_stack_init();
 
-    // Start execution.
-    //NRF_LOG_INFO("Beacon example started.");
-    //advertising_start();
+    uint32_t err_code = app_timer_start(m_timer_ble, ADV_INTERVAL, NULL);
 
-    //nrf_delay_ms(5000);
-
-    NRF_LOG_INFO("Beacon example started.");
-
-    for(;;) {
-      // Inicializamos características advertisement
-      advertising_init();
-
-      NRF_LOG_INFO("Beacon example started.");
-      advertising_start();
-
-      // Paramos el escaner
-      advertising_stop();
-
-      // MINUTOS_DELAY*60 segundos en standby (se suman los segundos retardo de envío advs)
-      nrf_delay_ms(NUM_ADVERTISEMENTS*ADV_INTERVAL_MS+MINUTOS_DELAY*60*1000);
-
-    }
-
-    // Enter main loop.
-
-    /*
-    for (;; )
+    while (true) 
     {
-        idle_state_handle();
+      idle_state_handle();
     }
-    */
 }
 
 
